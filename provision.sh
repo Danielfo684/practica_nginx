d6330492be6da410082aad0d6ed41a410033d8a6
Daniel Fontalva Ostio <145746911+Danielfo684@users.noreply.github.com>
2024-11-15 15:09:00 +0100

    cambiando nombres a las capturas y revisando la provisión

--- provision.sh
+++ provision.sh
@@ -1,5 +1,6 @@
 sudo apt update -y
-sudo apt install -y nginx git 
+sudo apt install -y nginx git vsftpd
+sudo apt-get upgrade -y
 
 sudo  systemctl enable nginx  
 sudo  systemctl start nginx    
@@ -10,55 +11,51 @@ sudo  mkdir -p /var/www/daninginx/html
 
 sudo  git clone https://github.com/cloudacademy/static-website-example /var/www/daninginx/html
 
-sudo  chown -R www-data:www-data /var/www/daninginx/html
-sudo  chmod -R 775 /var/www/daninginx
+sudo  chown -R www-data:www-data /var/www/
+sudo  chmod -R 775 /var/www/
 
 
-sudo cp /vagrant/daninginx /etc/nginx/sites-available/daninginx
+sudo cp /vagrant/daninginx /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/daninginx /etc/nginx/sites-enabled/
 
-
-sudo  nginx -t
-
-sudo  systemctl restart nginx
 
 
 #instalacion del segundo sitio web
 
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y vsftpd
+
+
+
+
+#permisos  y usuario FTP
 
 sudo adduser dani
-
 echo "dani:dani" | sudo chpasswd
 
 
-sudo mkdir /home/dani/ftp
-sudo chown vagrant:vagrant /home/dani/ftp
+sudo mkdir -p /home/dani/ftp
+sudo chown dani:dani /home/dani/ftp
 sudo chmod 775 /home/dani/ftp
 
 
-if [ ! -f /etc/ssl/certs/vsftpd.crt ]; then
-    sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.key -out /etc/ssl/certs/vsftpd.crt -subj "/C=ES/ST=Granada/L=Granada/O=IESZaidinVergeles/CN=192.168.10.15"
-fi
 
-cp /vagrant/vsftpd.conf /etc/vsftpd.conf
+#claves para el certificado
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.key -out /etc/ssl/certs/vsftpd.crt -subj "/C=ES/ST=Granada/L=Granada/O=Global Security/OU=IES ZAIDIN VERGELES/CN=exploreX.test"
+
+sudo cp -v /vagrant/vsftpd.conf /etc/vsftpd.conf
+
+sudo systemctl start vsftpd
 
 sudo usermod -aG www-data dani
 
-sudo mkdir -p  /var/www/danijugger
-sudo chown -R www-data:www-data /var/www/danijugger
-sudo chmod -R 775 /var/www/danijugger
-
-sudo systemctl restart vsftpd
-
-sudo git clone https://github.com/Danielfo684/danielfo684.github.io.git /var/www/danijugger
-
-sudo chmod -R 775 /var/www/danijugger
+#carpeta para FileZilla
+sudo mkdir -p  /var/www/danijugger/html
 
 cp /vagrant/danijugger /etc/nginx/sites-available/danijugger
 
-
 sudo ln -s /etc/nginx/sites-available/danijugger /etc/nginx/sites-enabled/
 sudo systemctl restart nginx
+sudo systemctl restart vsftpd
+
+
+
+
